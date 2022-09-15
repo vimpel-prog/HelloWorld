077d8449d22e9a6e1044d3b04b379ccd8861ef6d
Sergey <vimpel812@gmail.com>
2022-09-15 19:39:45 +0300

    Поменяли вывод

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         //Hello
-        System.out.println("Hello world!");
-        System.out.println("Good day");
+        System.out.println("Привет мир!");
     }
 }
